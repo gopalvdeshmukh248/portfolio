5e9750026e127d03cdf515d56797dc30222d73c9
gopaldeshamuha <your-email@example.com>
2025-10-05 10:36:59 -0700

    Fixed merge conflict in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/portfolio/', // Using your GitHub repository name
-=======
-  base: '/portfolio/', // Replace 'p1' with your repository name
->>>>>>> 2678aa3abc5b08d12b759513ea3035e448e1fe38
 })
